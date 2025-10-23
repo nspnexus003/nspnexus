ad9a79029078975a9cc842d181022f5e9c6c15f0
nspnexus003 <nspnexus003@gmail.com>
2025-10-23 22:56:23 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,17 @@ export default function RootLayout({
       <head>
         <meta name="author" content="NSP NEXUS" />
         <meta name="copyright" content="NSP NEXUS © 2025" />
+
+        {/* ---- Start: Favicon and Manifest Links ---- */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        {/* ---- End: Favicon and Manifest Links ---- */}
+
       </head>
       <body className="antialiased">
         <ErrorReporter />
